a920ab2aa762340ba4b630b87e16eaaa440bca00
Даня <diman100l@mail.ru>
2020-11-16 21:56:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+const select = document.querySelector('.filter__select')
+const testimonialsItem = document.querySelectorAll('.testimonials__item')
+const filterLinks = document.querySelectorAll('.filter__link')
+const filterDoctors = document.querySelector('.filter__doctors')
+const filterButtons = document.querySelector('.filter__buttons')
+const filterBtn = document.querySelectorAll('.filter__btn')
+
+const hideTestimonials = () => {
+  testimonialsItem.forEach(item => {
+    item.classList.remove('open')
+  })
+}
+
+const showTestimonials = () => {
+  testimonialsItem.forEach(item => {
+    item.classList.add('open')
+  })
+}
+
+const showDoctors = item => {
+  filterButtons.classList.add('close')
+  filterDoctors.classList.add('open')
+  filterLinks.forEach(link => {
+    let spec = link.dataset.spec
+    if (item.classList.contains(spec)) {
+      link.classList.add('selected')
+    }
+  })
+}
+
+
+const hideDoctors = () => {
+  filterButtons.classList.remove('close')
+  filterDoctors.classList.remove('open')
+}
+
+const chooseDoctor = () => {
+  showTestimonials()
+  select.addEventListener('change', function() {
+    
+    let value = this.value
+
+    testimonialsItem.forEach(item => {
+      if (item.classList.contains(value)) {
+
+        filterLinks.forEach(link => {
+          link.classList.remove('selected')
+        })
+        showDoctors(item)
+        hideTestimonials()
+        item.classList.add('open')
+
+      } else if (value === 'all') {
+        hideDoctors()
+        showTestimonials()
+      }
+    })
+
+  })
+
+}
+
+
+const showOtherTestimonials = () => {
+
+  testimonialsItem.forEach(item => {
+    
+    filterBtn.forEach(btn => {
+      let value = btn.dataset.value
+      btn.addEventListener('click', () => {
+        if (item.classList.contains(value)) {
+          hideTestimonials()
+          item.classList.add('open')
+        }
+      })
+    })
+
+  })
+}
+
+
+chooseDoctor()
+showOtherTestimonials()
